bbd1300409d8f19de5aef6716cdb299114a40e05
Bradford Knowlton <brad@ucanbonline.net>
2018-09-17 15:46:28 -0400

    displaying recent blog posts

--- functions.php
+++ functions.php
@@ -7,13 +7,13 @@ function themes_dir_add_rewrites() {
  
   global $wp_rewrite;
   $new_non_wp_rules = array(
-	'static/css/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/css/$1',
-	'static/js/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/js/$1',
-	'static/media/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/media/$1',
-  	'static/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/$1',
+  	'static/css/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/css/$1',
+  	'static/js/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/js/$1',
+  	'static/media/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/media/$1',
+    'static/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/$1',
     'build/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/$1',
     'src/(.*)'        => 'wp-content/themes/'. $theme_name . '/src/$1',
-    'public/(.*)'        => 'wp-content/themes/'. $theme_name . '/src/$1'
+    'public/(.*)'        => 'wp-content/themes/'. $theme_name . '/public/$1'
   );
   $wp_rewrite->non_wp_rules += $new_non_wp_rules;
 }
--- src/App.js
+++ src/App.js
@@ -3,17 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://x86virtualization.com/wp-json/wp/v2/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }));
+  }
+
   render() {
+
+    const posts = ( this.state.posts[0] !== undefined )?<Posts posts={this.state.posts} />:null;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to x86 Virtualization</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h2>Recent Blog Posts:</h2>
+          {posts}
+        </div>
       </div>
+    );
+  }
+}
+
+class Posts extends Component {
+  render(){
+
+    console.log(this.props.posts);
+
+    const listPosts = this.props.posts.map((post) =>
+      <li key={post.id}><h3><a href={post.slug}>{post.title.rendered}</a></h3><p className="body" dangerouslySetInnerHTML={{__html: post.excerpt.rendered }}></p></li>
+    );
+
+    return (
+      <ul>
+        {listPosts}
+      </ul>
     );
   }
 }
