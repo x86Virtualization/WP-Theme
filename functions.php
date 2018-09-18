e773a71b9f4f8a56d53c5a447786391ac9b23ea8
Bradmkjr <brad@ucanbonline.net>
2018-09-18 09:58:49 -0400

    added values to API

--- functions.php
+++ functions.php
@@ -1,10 +1,17 @@
 <?php
+/**
+ * The main template file
+ *
+ * @package WordPress
+ * @subpackage WP-Theme
+ * @since WP-Theme 1.1.6
+ */
 
 add_action('generate_rewrite_rules', 'themes_dir_add_rewrites');
- 
+
 function themes_dir_add_rewrites() {
   $theme_name = next(explode('/themes/', get_stylesheet_directory()));
- 
+
   global $wp_rewrite;
   $new_non_wp_rules = array(
   	'static/css/(.*)'       => 'wp-content/themes/'. $theme_name . '/build/static/css/$1',
@@ -16,4 +23,50 @@ function themes_dir_add_rewrites() {
     'public/(.*)'        => 'wp-content/themes/'. $theme_name . '/public/$1'
   );
   $wp_rewrite->non_wp_rules += $new_non_wp_rules;
+}
+
+// Add various fields to the JSON output
+function wpt_register_fields() {
+    // Add Author Name
+    register_rest_field( 'post',
+        'author_name',
+        array(
+            'get_callback'      => 'wpt_get_author_name',
+            'update_callback'   => null,
+            'schema'            => null
+        )
+    );
+    // Add Featured Image
+    register_rest_field( 'post',
+        'featured_image_src',
+        array(
+            'get_callback'      => 'wpt_get_image_src',
+            'update_callback'   => null,
+            'schema'            => null
+        )
+   );
+   // Add Published Date
+    register_rest_field( 'post',
+       'published_date',
+       array(
+           'get_callback'      => 'wpt_published_date',
+           'update_callback'   => null,
+           'schema'            => null
+       )
+    );
+}
+add_action( 'rest_api_init', 'wpt_register_fields' );
+
+function wpt_get_author_name( $object, $field_name, $request ) {
+    return get_the_author_meta( 'display_name' );
+}
+function wpt_get_image_src( $object, $field_name, $request ) {
+   if($object[ 'featured_media' ] == 0) {
+       return $object[ 'featured_media' ];
+   }
+    $feat_img_array = wp_get_attachment_image_src( $object[ 'featured_media' ], 'thumbnail', true );
+   return $feat_img_array[0];
+}
+function wpt_published_date( $object, $field_name, $request ) {
+    return get_the_time('F j, Y');
 }
